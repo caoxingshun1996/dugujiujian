88102a069cc80cc229cfe52cf00220053c601d08
岳不群 <caoxingshun@yeah.net>
2017-01-12 10:03:19 +0800

    update 02

--- src/com/atguigu/Dugujiujian.java
+++ src/com/atguigu/Dugujiujian.java
@@ -2,6 +2,11 @@ package com.atguigu;
 
 public class Dugujiujian {
 	public static void main(String[] args) {
-		System.out.println("01");
+		System.out.println("01 02");
+		System.out.println("01 02");
+		System.out.println("01 02");
+		System.out.println("01 02");
+		System.out.println("01 02");
+		
 	}
 }
